073329e24dc746bb9ed42fe4e07f062947ac73d9
BenG <57101218+Ben-Gendler@users.noreply.github.com>
2020-04-03 15:05:46 +0300

    Tree fixes and func implementations
    TODO:
    relation,find, SMART remove
    DONE:
    display and sub functions, internal func find, addmother/father

--- Tree.cpp
+++ Tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "FamilyTree.hpp"
 using namespace family;
+#define SPACES 10
 
 static node GetNewNode(string name)
 {
@@ -15,20 +16,68 @@ Tree::Tree(string name)
   root = GetNewNode(name);
 }
 
-//bfs dfs and duplications
 static node findByName(node root, string name)
 {
+	if (root == NULL)
+		return NULL;
+
   if (root->name == name)
   {
     return root;
   }
-  //TODO
+
+	if(root->father!=NULL)
+	{
+		return findByName(root->father,name);
+	}
+
+	if(root->mother!=NULL)
+	{
+		return findByName(root->mother,name);
+	}
+
   return NULL;
 }
 
-Tree Tree::addFather(string name, string father_name)
+Tree Tree::addFather(string child, string father_name)
 {
-  node relative = findByName(root, name);
-  relative->father = GetNewNode(father_name);
+  node CurrRoot = findByName(this->root, child);
+  CurrRoot->father = GetNewNode(father_name);
   return *this;
+}
+
+Tree Tree::addMother(string child, string mother_name)
+{
+  node CurrRoot = findByName(this->root, child);
+  CurrRoot->mother = GetNewNode(mother_name);
+  return *this;
+}
+
+void actualprint(node root,int spaceAmount)
+{
+	if (root == NULL)
+        return;
+
+spaceAmount += SPACES;
+actualprint(root->mother, spaceAmount);
+
+cout<<endl;
+
+for (int i = SPACES; i < spaceAmount; i++)
+	cout<<" ";
+
+cout<<root->name<<"\n";
+
+actualprint(root->father,spaceAmount);
+
+}
+
+void prettyprint(node root) //wrapper
+{
+	actualprint(root,0);
+}
+
+void Tree::display()
+{
+		prettyprint(this->root);
 }
