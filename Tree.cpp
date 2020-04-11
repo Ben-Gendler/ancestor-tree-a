5c959a5426e8211076fd7998eaf3f2fbe2f167ef
BenG <57101218+Ben-Gendler@users.noreply.github.com>
2020-04-11 04:42:03 +0300

    beautify

--- Tree.cpp
+++ Tree.cpp
@@ -2,107 +2,105 @@
 #include "FamilyTree.hpp"
 using namespace family;
 
-static node GetNewNode(string name,int depth, gender family_role)
+static node GetNewNode(string name, int depth, gender family_role)
 {
-	node newMember = new struct tree_node(depth+1,family_role);
+	node newMember = new struct tree_node(depth + 1, family_role);
 	newMember->name = name;
 	newMember->mother = newMember->father = NULL;
 	return newMember;
 }
 
-
 Tree::Tree(string name)
 {
-  root = GetNewNode(name,0,SELF);
+	root = GetNewNode(name, 0, SELF);
 }
 
-tree_node::tree_node(int depth,gender family_role)
+tree_node::tree_node(int depth, gender family_role)
 {
-		this->depth = depth;
-		this->family_role = family_role;
+	this->depth = depth;
+	this->family_role = family_role;
 }
 
 static node findByName(node root, string name)
 {
-if (!root)
-    return NULL;
+	if (!root)
+		return NULL;
 
-if (root->name == name)
-    return root;
-node temp = findByName(root->father,name);
+	if (root->name == name)
+		return root;
+	node temp = findByName(root->father, name);
 
-if(temp)
-	return temp;
+	if (temp)
+		return temp;
 
-return findByName(root->mother,name);
+	return findByName(root->mother, name);
 
 }
 
 Tree Tree::addFather(string child, string father_name)
 {
-node CurrRoot = findByName(this->root, child);
-CurrRoot->father = GetNewNode(father_name,CurrRoot->depth,FATHER);
+	node CurrRoot = findByName(this->root, child);
+	CurrRoot->father = GetNewNode(father_name, CurrRoot->depth, FATHER);
 
-	return *this;
+	return * this;
 }
 
 Tree Tree::addMother(string child, string mother_name)
 {
-node CurrRoot = findByName(this->root, child);
-CurrRoot->mother = GetNewNode(mother_name,CurrRoot->depth,MOTHER);
+	node CurrRoot = findByName(this->root, child);
+	CurrRoot->mother = GetNewNode(mother_name, CurrRoot->depth, MOTHER);
 
-  return *this;
+	return * this;
 }
 
-
-void prettyprint(node root) //wrapper func
+void prettyprint(node root)	//wrapper func
 {
-if(!root)
-	return;
+	if (!root)
+		return;
 
-prettyprint(root->mother);
+	prettyprint(root->mother);
 
-for(int i=0;i<root->depth-1;i++)
-	std::cout << "        ";
+	for (int i = 0; i < root->depth - 1; i++)
+		std::cout << "        ";
 
-std::cout << root->name << "\n\n";
+	std::cout << root->name << "\n\n";
 
-prettyprint(root->father);
+	prettyprint(root->father);
 
 }
 
 void Tree::display()
 {
-prettyprint(this->root);
+	prettyprint(this->root);
 }
 
-static node findRelation(node root,string family_member_name)
+static node findRelation(node root, string family_member_name)
 {
-if(!root)
-	return NULL;
+	if (!root)
+		return NULL;
 
-if(root->name == family_member_name)
+	if (root->name == family_member_name)
 		return root;
 
-node tempNode = findRelation(root->father,family_member_name);
+	node tempNode = findRelation(root->father, family_member_name);
 
-if(tempNode)
-	return tempNode;
+	if (tempNode)
+		return tempNode;
 
-	return findRelation(root->mother,family_member_name);
+	return findRelation(root->mother, family_member_name);
 }
 
 static string nodeToString(node root)
 {
-	if(!root)
+	if (!root)
 		return "unrelated";
 
-	if(root->depth == 1)
+	if (root->depth == 1)
 		return "me";
 
-	if(root->depth == 2)
+	if (root->depth == 2)
 	{
-		if(root->family_role == FATHER)
+		if (root->family_role == FATHER)
 			return "father";
 		return "mother";
 	}
@@ -110,66 +108,64 @@ static string nodeToString(node root)
 	{
 		string greatSum = "grandmother";
 
-		if(root->family_role == FATHER)
+		if (root->family_role == FATHER)
 			greatSum = "grandfather";
 
-		for(int i=0; i<root->depth-3;i++)
-			greatSum= "great-" + greatSum;
+		for (int i = 0; i < root->depth - 3; i++)
+			greatSum = "great-" + greatSum;
 
 		return greatSum;
 	}
 }
-string Tree::relation(string family_member_name) //NOT DONE
+string Tree::relation(string family_member_name)	//NOT DONE
 {
-node temp = findRelation(this->root,family_member_name);
+	node temp = findRelation(this->root, family_member_name);
 
 	return nodeToString(temp);
 }
-
 
 static void deleteSubTree(node CurrRoot)
 {
 	if (CurrRoot == NULL)
 		return;
 
-//Delete above subtrees//
-deleteSubTree(CurrRoot->mother);
-deleteSubTree(CurrRoot->father);
-CurrRoot->mother=CurrRoot->father=NULL;
-//Delete the required node itself
-cout << "\n deleting " << CurrRoot->name << " from the tree\n";
+	//Delete above subtrees//
+	deleteSubTree(CurrRoot->mother);
+	deleteSubTree(CurrRoot->father);
+	CurrRoot->mother = CurrRoot->father = NULL;
+	//Delete the required node itself
+	cout << "\n deleting " << CurrRoot->name << " from the tree\n";
 
-delete(CurrRoot);
+	delete(CurrRoot);
 
 }
 
-
 void Tree::remove(string removeAbove)
 {
-	deleteSubTree(findByName(this->root,removeAbove));
+	deleteSubTree(findByName(this->root, removeAbove));
 }
 
 node findByRole(node root, string family_role)
 {
 	if (!root)
-	    return NULL;
+		return NULL;
 
 	if (nodeToString(root) == family_role)
-	    return root;
+		return root;
 
-	node temp = findByRole(root->father,family_role);
+	node temp = findByRole(root->father, family_role);
 
-	if(temp)
+	if (temp)
 		return temp;
 
-temp = findByRole(root->mother,family_role);
+	temp = findByRole(root->mother, family_role);
 	return temp;
 
 }
 string Tree::find(string family_role)
 {
-	node result = findByRole(this->root,family_role);
-	if(result)
+	node result = findByRole(this->root, family_role);
+	if (result)
 		return result->name;
 
 	return "unrelated";
